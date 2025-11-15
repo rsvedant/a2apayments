d23491a72266728cbe86f997bd0cae228614d8f4
Vedant Singh <iamvedantsingh1407@gmail.com>
2025-11-15 10:33:00 -0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../index.css";
+import "./globals.css";
 import Providers from "@/components/providers";
 import Header from "@/components/header";
 
